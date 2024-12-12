888824bdf77e08c9e350af24a2a041bf5b4cb300
nwizugbesamson <samson6398@gmail.com>
2024-12-12 19:31:15 +0000

    add application, infrastructure, persistence to api

--- CareerBoostAI.API/Program.cs
+++ CareerBoostAI.API/Program.cs
@@ -1,19 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+using CareerBoostAI.Application;
+using CareerBoostAI.Infrastructure;
+using CareerBoostAI.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddApplication();
+    builder.Services.AddInfrastructure();
+    builder.Services.AddPersistence();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+}
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
