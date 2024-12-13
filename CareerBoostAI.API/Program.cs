8920765826049423f6eadab4fc080b748a5e706c
nwizugbesamson <samson6398@gmail.com>
2024-12-13 00:03:05 +0000

    enable api route automatic mapping

--- CareerBoostAI.API/Program.cs
+++ CareerBoostAI.API/Program.cs
@@ -1,5 +1,7 @@
 
 
+using System.Reflection;
+using CareerBoostAI.API.Extensions;
 using CareerBoostAI.Application;
 using CareerBoostAI.Infrastructure;
 using CareerBoostAI.Persistence;
@@ -11,10 +13,11 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services.AddPersistence();
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
+    builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
 }
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -22,29 +25,7 @@ if (app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapEndpoints();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
