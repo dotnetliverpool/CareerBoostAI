b6792e9b27be5599b0b19b2532fff33207874ed4
nwizugbesamson <samson6398@gmail.com>
2024-12-17 15:09:31 +0000

    Add health check endpoint

---
+++ CareerBoostAI.Functions/Functions/HealthCheck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+
+namespace CareerBoostAI.Functions.Functions;
+
+public class HealthCheck
+{
+    private readonly ILogger<HealthCheck> _logger;
+    
+    public HealthCheck(ILogger<HealthCheck> logger)
+    {
+        _logger = logger;
+    }
+    
+    
+    [FunctionName("Up")]
+    [OpenApiOperation(operationId: "HealthCheck", tags: new[] { "Health" }, Summary = "Perform a health check", Description = "Returns the health status of the application.")]
+    [OpenApiResponseWithBody(statusCode: System.Net.HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(ApplicationHealthCheck), Description = "The health status response.")]
+    public async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get",  Route = "health-check")] HttpRequest req)
+    {
+        _logger.LogInformation("Health Check Run");
+        string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        var response = new ApplicationHealthCheck("Healthy", "Health check passed successfully.");
+        return new OkObjectResult(response);
+    }
+}
+
+record ApplicationHealthCheck(string Status, string Message);
--- CareerBoostAI.Functions/Startup.cs
+++ CareerBoostAI.Functions/Startup.cs
@@ -1,6 +1,31 @@
-﻿namespace CareerBoostAI.Functions;
+﻿using CareerBoostAI.Application;
+using CareerBoostAI.Functions;
+using CareerBoostAI.Infrastructure;
+using CareerBoostAI.Persistence;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
-public class Startup
+[assembly:FunctionsStartup(typeof(Startup))]
+namespace CareerBoostAI.Functions;
+
+public class Startup : FunctionsStartup
 {
-    
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        var config = BuildConfiguration(builder.GetContext().ApplicationRootPath);
+        builder.Services.AddApplication();
+        builder.Services.AddInfrastructure();
+        builder.Services.AddPersistence();
+    }
+
+    private IConfiguration BuildConfiguration(string applicationRootPath)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(applicationRootPath)
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+        return configuration;
+    }
 }
