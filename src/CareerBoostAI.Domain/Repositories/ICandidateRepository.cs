fe7a2025755500897bbd7ff850728b9d84f971c2
nwizugbesamson <samson6398@gmail.com>
2024-12-18 13:40:50 +0000

    create candidate repository

---
+++ src/CareerBoostAI.Domain/Repositories/ICandidateRepository.cs
@@ -0,0 +1,11 @@
+﻿using CareerBoostAI.Domain.Entities;
+using CareerBoostAI.Domain.ValueObjects;
+
+namespace CareerBoostAI.Domain.Repositories;
+
+public interface ICandidateRepository
+{
+    Task<Candidate> GetAsync(CandidateId id);
+    Task AddAsync(Candidate candidate);
+    Task UpdateAsync(Candidate candidate);
+}
