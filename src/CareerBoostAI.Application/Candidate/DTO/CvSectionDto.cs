9834f3f2555804cb3c223c09e1070c271be9b3ec
nwizugbesamson <samson6398@gmail.com>
2025-01-13 23:53:54 +0000

    add directories to model towards 3 bounded contexts

--- src/CareerBoostAI.Application/Candidate/DTO/CvSectionDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CareerBoostAI.Application.Candidate.DTO;
-
-public class CvSectionDto
-{
-    public string SectionName { get; set; }
-    public uint SequenceIndex { get; set; }
-    public List<CvSectionItemDto> Items { get; set; } = new();
-}
