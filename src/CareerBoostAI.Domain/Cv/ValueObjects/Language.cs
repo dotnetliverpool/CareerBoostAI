6e14ffa5826b8c829bc878cc17c9f18100d89f73
nwizugbesamson <samson6398@gmail.com>
2025-01-22 16:14:42 +0000

    Move language and skill value object to Cv context

---
+++ src/CareerBoostAI.Domain/Cv/ValueObjects/Language.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+using CareerBoostAI.Domain.Common.Exceptions;
+using CareerBoostAI.Domain.ValueObjects;
+
+namespace CareerBoostAI.Domain.Cv.ValueObjects;
+
+public class Language : ValueObject
+{
+    private static readonly ConcurrentDictionary<string, Language> FlyweightCache = new();
+    public string Value { get; }
+
+    private Language(string value)
+    {
+        Value = value;
+    }
+    
+    public Language() {}
+
+    public static Language Create(string value)
+    {
+        value.ThrowIfNullOrEmpty(nameof(Language));
+        var result = value.Trim().ToLower();
+        return FlyweightCache.GetOrAdd(result, key => new Language(key));;
+    }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
---
+++ src/CareerBoostAI.Domain/Cv/ValueObjects/Skill.cs
@@ -0,0 +1,28 @@
+﻿using CareerBoostAI.Domain.Common.Exceptions;
+using CareerBoostAI.Domain.ValueObjects;
+
+namespace CareerBoostAI.Domain.Cv.ValueObjects;
+
+public class Skill : ValueObject
+{
+    public string Value { get; }
+
+    private Skill(string value)
+    {
+        Value = value;
+    }
+    
+    public Skill() {}
+
+    public static Skill Create(string value)
+    {
+        value.ThrowIfNullOrEmpty(nameof(Skill));
+        var result = value.Trim().ToLower();
+        return new(result);
+    }
+
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Value;
+    }
+}
