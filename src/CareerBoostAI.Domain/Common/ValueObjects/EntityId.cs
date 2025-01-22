d1a24081cd7e50c5797d3c7498b69d1c4ba31c27
nwizugbesamson <samson6398@gmail.com>
2025-01-22 15:46:22 +0000

    create Base EntityId class

---
+++ src/CareerBoostAI.Domain/Common/ValueObjects/EntityId.cs
@@ -0,0 +1,18 @@
+﻿using CareerBoostAI.Domain.Common.Exceptions;
+
+namespace CareerBoostAI.Domain.Common.ValueObjects;
+
+public class EntityId
+{
+    private EntityId(Guid value)
+    {
+        Value = value;
+    }
+
+    public Guid Value { get; }
+    public static EntityId Create(Guid value)
+    {
+        value.ThrowIfNull();
+        return new(value);
+    }
+}
