fcd3712b25f7a34302ab0f81aac5d04e9d438efe
nwizugbesamson <samson6398@gmail.com>
2025-01-22 15:44:03 +0000

    move professional entry classes to CV aggregate

---
+++ src/CareerBoostAI.Domain/Cv/Entities/Education.cs
@@ -0,0 +1,43 @@
+﻿using CareerBoostAI.Domain.Candidate.Cv.ValueObjects;
+using CareerBoostAI.Domain.Candidate.CvEntity.ValueObjects;
+
+namespace CareerBoostAI.Domain.Cv.Entities;
+
+public class Education : Candidate.CvEntity.ProfessionalEntry
+{
+    public EducationGrade Grade { get;  }
+    
+    private Education(
+        ProfessionalEntryId id,
+        OrganisationName organisationName,
+        Location location,
+        Period timePeriod,
+        EducationGrade educationGrade,
+        SequenceIndex sequenceIndex)
+        : base(id, organisationName, location, timePeriod, sequenceIndex)
+    {
+        Grade = educationGrade;
+    }
+    
+    public Education() {}
+
+   
+
+    public static Education Create(
+        Guid id,
+        string organisationName,
+        string city, string country,
+        DateOnly startDate, DateOnly? endDate,
+        string program, string grade, uint index)
+    {
+        var edId = ProfessionalEntryId.Create(id);
+        var orgName = OrganisationName.Create(organisationName);
+        var location = Candidate.CvEntity.ValueObjects.Location.Create(city, country);
+        var timePeriod = Period.Create(startDate, endDate);
+        var gradeDomain = EducationGrade.Create(program, grade);
+        var sequenceIndex = SequenceIndex.Create(index);
+        return new(edId, orgName, location, timePeriod, gradeDomain, sequenceIndex);
+    }
+
+    
+}
---
+++ src/CareerBoostAI.Domain/Cv/Entities/WorkExperience.cs
@@ -0,0 +1,43 @@
+﻿using CareerBoostAI.Domain.Candidate.Cv.ValueObjects;
+using CareerBoostAI.Domain.Candidate.CvEntity.ValueObjects;
+
+namespace CareerBoostAI.Domain.Cv.Entities;
+
+public class WorkExperience : Candidate.CvEntity.ProfessionalEntry
+{
+    private WorkExperience(
+        ProfessionalEntryId id,
+        OrganisationName organisationName,
+        Location location,
+        Period timePeriod,
+        Description description,
+        SequenceIndex sequenceIndex)
+        : base(id, organisationName, location, timePeriod, sequenceIndex)
+    {
+        Description = description;
+    }
+    
+    public WorkExperience() {}
+    
+    public Description Description { get; }
+
+    public static WorkExperience Create(
+        Guid id,
+        string organisationName,
+        string city, string country,
+        DateOnly startDate, DateOnly? endDate,
+        string description, uint index)
+    {
+        var expId = ProfessionalEntryId.Create(id);
+        var orgName = OrganisationName.Create(organisationName);
+        var location = Candidate.CvEntity.ValueObjects.Location.Create(city, country);
+        var timePeriod = Period.Create(startDate, endDate);
+        var descriptionDomain = Candidate.CvEntity.ValueObjects.Description.Create(description);
+        var sequenceIndex = SequenceIndex.Create(index);
+        return new(expId, orgName, location, timePeriod, descriptionDomain, sequenceIndex);
+    }
+    
+    
+    
+    
+}
