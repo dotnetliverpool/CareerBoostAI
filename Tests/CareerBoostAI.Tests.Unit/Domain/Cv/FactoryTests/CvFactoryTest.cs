a09ed8976116f107ba6d1970dda214e1b55e4ffd
nwizugbesamson <samson6398@gmail.com>
2025-01-24 17:48:58 +0000

    write skill factory tests

--- Tests/CareerBoostAI.Tests.Unit/Domain/Cv/FactoryTests/CvFactoryTest.cs
+++ Tests/CareerBoostAI.Tests.Unit/Domain/Cv/FactoryTests/CvFactoryTest.cs
@@ -1,14 +1,37 @@
-﻿using CareerBoostAI.Domain.Common.ValueObjects;
-using CareerBoostAI.Domain.CvContext.Factory;
-using CareerBoostAI.Domain.CvContext.ValueObjects;
+﻿using CareerBoostAI.Domain.CvContext.ValueObjects;
 using Shouldly;
 using Xunit;
-using DateOnly = System.DateOnly;
 
-namespace CareerBoostAI.Tests.Unit.Domain.Cv;
+namespace CareerBoostAI.Tests.Unit.Domain.Cv.FactoryTests;
 
 public class CvFactoryTest : BaseCvTest
 {
+    
+    [Fact]
+    public void Skill_Create_ShouldReturnSkill_WhenPassedValidData()
+    {
+        // Arrange
+        var value = "C#";
+
+        // Act
+        var skill = Skill.Create(value);
+
+        // Assert
+        skill.ShouldNotBeNull();
+        skill.Value.ShouldBe(value.ToLower());  // Should convert value to lowercase
+    }
+
+    [Theory]
+    [InlineData(null)]  // Null value
+    [InlineData("")]    // Empty value
+    public void Skill_Create_ShouldThrowArgumentNullException_WhenInvalidDataIsPassed(string value)
+    {
+        // Act & Assert
+        var exception = Record.Exception(() => Skill.Create(value));
+
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<ArgumentNullException>();
+    }
     [Fact]
     public void CreateFromData_ShouldReturnValidCv_WhenPassedValidData()
     {
