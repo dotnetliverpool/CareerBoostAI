a668e8c106fe01bd8b3b4347d62503dddb939bd3
nwizugbesamson <samson6398@gmail.com>
2025-01-24 14:50:06 +0000

    Create name value object

---
+++ src/CareerBoostAI.Domain/CandidateContext/ValueObjects/Name.cs
@@ -0,0 +1,28 @@
+﻿using CareerBoostAI.Domain.Common.Exceptions;
+using CareerBoostAI.Domain.ValueObjects;
+
+namespace CareerBoostAI.Domain.CandidateContext.ValueObjects;
+
+public class Name : ValueObject
+{
+    public string FirstName { get;  }
+    public string LastName { get;  }
+
+    private Name(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static Name Create(string firstName, string lastName)
+    {
+        firstName.ThrowIfNullOrEmpty($"{nameof(Name)}.FirstName");
+        lastName.ThrowIfNullOrEmpty($"{nameof(Name)}.LastName");
+        return new Name(firstName, lastName);
+    }
+    protected override IEnumerable<object> GetAtomicValues()
+    {
+        yield return FirstName;
+        yield return LastName;
+    }
+}
