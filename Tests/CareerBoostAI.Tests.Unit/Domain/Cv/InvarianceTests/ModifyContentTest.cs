ef4641396b81e6c87f1ca36ca2d200da0c68a0f0
nwizugbesamson <samson6398@gmail.com>
2025-01-25 14:19:52 +0000

    write Cv.UpdateSkills test

--- Tests/CareerBoostAI.Tests.Unit/Domain/Cv/InvarianceTests/ModifyContentTest.cs
+++ Tests/CareerBoostAI.Tests.Unit/Domain/Cv/InvarianceTests/ModifyContentTest.cs
@@ -1,4 +1,5 @@
 ﻿using CareerBoostAI.Domain.Common.ValueObjects;
+using CareerBoostAI.Domain.CvContext.Factory;
 using CareerBoostAI.Domain.CvContext.ValueObjects;
 using Shouldly;
 using Xunit;
@@ -33,4 +34,64 @@ public class ModifyContentTest : BaseCvTest
         cv.Summary.ShouldNotBeNull();
         cv.Summary.Value.ShouldBe(expectedSummary); 
     }
+    
+    [Fact]
+    public void UpdateSkills_ShouldAddSkills_WhenSkillsWereEmpty()
+    {
+        // Arrange
+        var factory = GetCvFactory();
+        var data = new CvData
+        {
+            Summary = GetValidCvSummary(),
+            CandidateEmail = "candidate@cv.com",
+            Educations = GetValidCvEducations(),
+            Experiences = GetValidCvExperiences(),
+            Languages = GetValidCvLanguages(),
+            Skills = []
+        };
+
+        var skillsData = new [] {"C#", "ASP.NET Core"}; 
+
+        var cv = factory.CreateFromData(data); 
+
+        // Act
+        cv.UpdateSkills(skillsData); // Call the UpdateSkills method with new skills
+
+        // Assert
+        cv.Skills.Count.ShouldBe(2); 
+        cv.Skills.ShouldContain(Skill.Create("C#"));
+        cv.Skills.ShouldContain(Skill.Create("ASP.NET Core"));
+    }
+    
+    [Fact]
+    public void UpdateSkills_ShouldIntersectWithExistingSkills_WhenSkillsAreAdded()
+    {
+        // Arrange
+        var factory = GetCvFactory();
+        var data = new CvData
+        {
+            Summary = GetValidCvSummary(),
+            CandidateEmail = "candidate@cv.com",
+            Educations = GetValidCvEducations(),
+            Experiences = GetValidCvExperiences(),
+            Languages = GetValidCvLanguages(),
+            Skills = ["C#", "ASP.NET Core"]
+        };
+
+        var skillsData = new [] {"C#", "ASP.NET Core", "SQL"}; // New skills, with overlap (one common skill)
+
+        var cv = factory.CreateFromData(data); 
+
+        // Act
+        cv.UpdateSkills(skillsData); 
+
+        // Assert
+        cv.Skills.Count.ShouldBe(3); // Assert that the total number of skills is 3
+        cv.Skills.ShouldContain(Skill.Create("C#")); // Existing skill
+        cv.Skills.ShouldContain(Skill.Create("ASP.NET Core")); // Existing skill
+        cv.Skills.ShouldContain(Skill.Create("SQL")); // New skill
+    }
+    
+
+
 }
