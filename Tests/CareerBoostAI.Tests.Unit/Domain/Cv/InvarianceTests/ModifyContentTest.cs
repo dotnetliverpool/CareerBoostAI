48b86000c0489d8ac30728545b5f31a447755f5d
nwizugbesamson <samson6398@gmail.com>
2025-01-25 15:32:24 +0000

    write test for cv update experiences

--- Tests/CareerBoostAI.Tests.Unit/Domain/Cv/InvarianceTests/ModifyContentTest.cs
+++ Tests/CareerBoostAI.Tests.Unit/Domain/Cv/InvarianceTests/ModifyContentTest.cs
@@ -1,4 +1,5 @@
-﻿using CareerBoostAI.Domain.Common.ValueObjects;
+﻿using CareerBoostAI.Domain.Common.Exceptions;
+using CareerBoostAI.Domain.Common.ValueObjects;
 using CareerBoostAI.Domain.CvContext.Factory;
 using CareerBoostAI.Domain.CvContext.ValueObjects;
 using Shouldly;
@@ -64,7 +65,7 @@ public class ModifyContentTest : BaseCvTest
     }
     
     [Fact]
-    public void UpdateSkills_ShouldNotCreateDuplicateSkills()
+    public void UpdateSkills_ShouldReplaceSkills_WhenValidDataIsProvided()
     {
         // Arrange
         var factory = GetCvFactory();
@@ -103,7 +104,7 @@ public class ModifyContentTest : BaseCvTest
             CandidateEmail = "candidate@cv.com",
             Educations = GetValidCvEducations(),
             Experiences = GetValidCvExperiences(),
-            Languages = Enumerable.Empty<string>(),  
+            Languages = [],  
             Skills = GetValidCvSkills(2)
         };
 
@@ -121,7 +122,7 @@ public class ModifyContentTest : BaseCvTest
     }
 
     [Fact]
-    public void UpdateLanguages_ShouldRemoveLanguagesThatAreNotInNewList()
+    public void UpdateLanguages_ShouldReplaceLanguages_WhenValidDataIsProvided()
     {
         // Arrange
         var factory = GetCvFactory();
@@ -150,7 +151,89 @@ public class ModifyContentTest : BaseCvTest
         cv.Languages.ShouldNotContain(Language.Create("French")); 
         cv.Languages.ShouldNotContain(Language.Create("German")); 
     }
+    
+    [Fact]
+    public void UpdateExperiences_ShouldReplaceExperiences_WhenValidDataIsProvided()
+    {
+        // Arrange
+        var factory = GetCvFactory();
+        var data = new CvData
+        {
+            Summary = GetValidCvSummary(),
+            CandidateEmail = "candidate@cv.com",
+            Educations = GetValidCvEducations(),
+            Experiences = GetValidCvExperiences(),
+            Languages = GetValidCvLanguages(),
+            Skills = GetValidCvSkills(),
+        };
 
+        var cv = factory.CreateFromData(data);
 
+        var newExperiences = new[]
+        {
+            new ExperienceData
+            {
+                OrganisationName = "New Company 1", City = "City1", Country = "Country1", 
+                StartDate = DateOnly.Parse("2019-03-01"), EndDate = DateOnly.Parse("2021-03-01"), 
+                Description = "Experience 1", Index = 1
+            },
+            new ExperienceData
+            {
+                OrganisationName = "New Company 2", City = "City2", Country = "Country2", 
+                StartDate = DateOnly.Parse("2016-03-01"), EndDate = DateOnly.Parse("2017-03-01"), 
+                Description = "Experience 2", Index = 2
+            }
+        };
+
+        // Act
+        cv.UpdateExperiences(newExperiences);
+
+        // Assert
+        cv.Experiences.Count.ShouldBe(2);  // New experiences should be added
+        cv.Experiences.ElementAt(0).OrganisationName.ShouldBe(OrganisationName.Create("New Company 1"));
+        cv.Experiences.ElementAt(0).OrganisationName.ShouldBe(OrganisationName.Create("New Company 2"));
+    }
+
+    [Fact]
+    public void UpdateExperiences_ShouldThrowException_WhenSequenceIsInvalid()
+    {
+        // Arrange
+        var factory = GetCvFactory();
+        var data = new CvData
+        {
+            Summary = GetValidCvSummary(),
+            CandidateEmail = "candidate@cv.com",
+            Educations = GetValidCvEducations(),
+            Experiences = GetValidCvExperiences(),
+            Languages = GetValidCvLanguages(),
+            Skills = GetValidCvSkills(),
+        };
+
+        var cv = factory.CreateFromData(data);
+
+        var invalidExperiences = new[]
+        {
+            new ExperienceData
+            {
+                OrganisationName = "New Company 1", City = "City1", Country = "Country1",
+                StartDate = DateOnly.Parse("2019-03-01"), EndDate = DateOnly.Parse("2021-03-01"),
+                Description = "Experience 1", Index = 1
+            },
+            new ExperienceData
+            {
+                OrganisationName = "New Company 2", City = "City2", Country = "Country2",
+                StartDate = DateOnly.Parse("2016-03-01"), EndDate = DateOnly.Parse("2017-03-01"),
+                Description = "Experience 2", Index = 5
+            }
+        };
+
+        // Act
+        var exception = Record.Exception(() => cv.UpdateExperiences(invalidExperiences));
+
+        // Assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<ProfessionalEntrySequenceInvalidException>();
+
+    }
 
 }
