31168061ec4c9d63e3c18763a57e3e2abb4f443d
nwizugbesamson <samson6398@gmail.com>
2025-03-16 14:01:35 +0000

    load design time db context factory connection string from environment variables

--- src/CareerBoostAI.Infrastructure/EF/Contexts/AppDbContextFactory.cs
+++ src/CareerBoostAI.Infrastructure/EF/Contexts/AppDbContextFactory.cs
@@ -8,30 +8,18 @@ namespace CareerBoostAI.Infrastructure.EF.Contexts;
 
 internal class AppDbContextFactory : IDesignTimeDbContextFactory<CareerBoostReadDbContext>
 {
-    
-    
-    
     public CareerBoostReadDbContext CreateDbContext(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+            .AddEnvironmentVariables() 
+            .Build();
+        var mySqlOptions = configuration.GetOptions<MySqlOptions>("Database:MySql");
         
+        var severVersion = new MySqlServerVersion(new Version(mySqlOptions.ServerVersion));
         var optionsBuilder = new DbContextOptionsBuilder<CareerBoostReadDbContext>();
-
-        if (!optionsBuilder.IsConfigured) // Only configure if EF hasn't already set it
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("dev.settings.json", optional:true, reloadOnChange: true)
-                .AddJsonFile("settings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables()
-                .Build();
-            var mySqlOptions = configuration.GetOptions<MySqlOptions>("Database:MySql");  
-            var serverVersion = new MySqlServerVersion(new Version(mySqlOptions.ServerVersion));
-            
-            optionsBuilder.UseMySql(
-                connectionString: mySqlOptions.ConnectionString,
-                serverVersion: serverVersion);
-        }
+        optionsBuilder
+            .UseMySql(connectionString: mySqlOptions.ConnectionString, serverVersion: severVersion);
+        
 
         return new CareerBoostReadDbContext(optionsBuilder.Options);
     }
